8fd174d1beb7d5af1bc10a7f4d994e430eda76d5
krishnagupta-prog <krishnagupta-prog@users.noreply.github.com>
2024-02-28 09:37:41 +0530

    task 1 from dev

--- GITProj01/src/main/java/com/nt/main/AppTest.java
+++ GITProj01/src/main/java/com/nt/main/AppTest.java
@@ -1,8 +1,14 @@
 package com.nt.main;
 
+import com.nt.service.ArithmeticOperation;
+
 public class AppTest {
 
 	public static void main(String[] args) {
+		
+		ArithmeticOperation ar= new ArithmeticOperation();
+		System.out.println("Sum:: "+ar.sum(300, 100));
+		System.out.println("Sub:: "+ar.sub(300, 100));
 		
 	}
 	
---
+++ GITProj01/src/main/java/com/nt/service/ArithmeticOperation.java
@@ -0,0 +1,13 @@
+package com.nt.service;
+
+public class ArithmeticOperation {
+
+	public int sum(int x, int y) {
+		return x+y;
+	}
+	
+	public int sub(int x, int y) {
+		return x-y;
+	}
+	
+}
